1e180b7b4b86815d8626f8556ad48b47d4fb9b05
Rakno7 <102826596+Rakno7@users.noreply.github.com>
2022-04-30 14:24:13 -0700

    Add files via upload

---
+++ Sedig_Website/Sedig_ObjectivesPage/main.js
@@ -0,0 +1,43 @@
+//-----------------Bradley Alex Sedig-------------------
+const Homebutton = document.getElementById("Home")
+const Portfoliobutton = document.getElementById("Portfolio")
+const Gamesbutton = document.getElementById("Games")
+const Objectivesbutton = document.getElementById("Objectives")
+const FlightGbutton = document.getElementById("FlightG")
+
+//this function runs on an interval and checks for onclick events from active buttons and runs the appropriate function
+
+      Homebutton.onclick = LoadHome
+      Portfoliobutton.onclick = LoadPortfolio
+      Gamesbutton.onclick = LoadGames
+      Objectivesbutton.onclick = LoadObjectives
+      FlightGbutton.onclick = LoadFlightSim
+
+function LoadHome()
+{
+    window.location.href = "https://rakno7.github.io/Sedig_Website/";
+}
+function LoadPortfolio()
+{
+    window.location.href = "https://rakno7.github.io/Sedig_Website/Sedig_PortfolioPage/";
+}
+function LoadGames()
+{
+    window.location.href = "https://rakno7.github.io/Sedig_Website/Sedig_GamesPage/";
+}
+function LoadObjectives()
+{
+    window.location.href = "https://rakno7.github.io/Sedig_Website/Sedig_ObjectivesPage/";
+}
+function LoadFlightSim()
+{
+    window.open("https://rakno7.github.io/Sedig_Website/Sedig_FlightSim/");
+    //window.location.href = "https://rakno7.github.io/Sedig_Website/FlightSim/";
+}
+
+
+
+
+
+
+
